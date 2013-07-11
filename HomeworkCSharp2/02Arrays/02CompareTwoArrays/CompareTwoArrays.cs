26594707e8822ca74aab1284829f35bee7d0ea7c
Yordan Bankov <bankoff@mail.bg>
2013-07-11 21:35:58 +0300

    ArraysNew02

--- HomeworkCSharp2/02Arrays/02CompareTwoArrays/CompareTwoArrays.cs
+++ HomeworkCSharp2/02Arrays/02CompareTwoArrays/CompareTwoArrays.cs
@@ -7,23 +7,30 @@ class CompareTwoArrays
 {
     static void Main()
     {
-        //declaring the array size 
-        uint sizeOfArrays;
+        //declaring the arrays size and read arrays elements from the console
+        uint sizeOfArray1;
         do
         {
-            Console.Write("Please enter the size of arrays: ");
+            Console.Write("Please enter the size of first array: ");
         }
-        while (!uint.TryParse(Console.ReadLine(), out sizeOfArrays));
+        while (!uint.TryParse(Console.ReadLine(), out sizeOfArray1));
 
-        //read arrays elements from the console
-        string[] arrayOne = new string[sizeOfArrays];
-        string[] arrayTwo = new string[sizeOfArrays];
-        for (int i = 0; i < sizeOfArrays; i++)
+        string[] arrayOne = new string[sizeOfArray1];
+        for (int i = 0; i < sizeOfArray1; i++)
         {
             Console.Write("Please enter {0} element for the first array: ", i + 1);
             arrayOne[i] = Console.ReadLine();
         }
-        for (int i = 0; i < sizeOfArrays; i++)
+
+        uint sizeOfArray2;
+        do
+        {
+            Console.Write("Please enter the size of second array: ");
+        }
+        while (!uint.TryParse(Console.ReadLine(), out sizeOfArray2));
+
+        string[] arrayTwo = new string[sizeOfArray2];
+        for (int i = 0; i < sizeOfArray2; i++)
         {
             Console.Write("Please enter {0} element for the second array: ", i + 1);
             arrayTwo[i] = Console.ReadLine();
@@ -31,7 +38,7 @@ class CompareTwoArrays
 
         //verifying the identity of the components of the arrays element by element
         int counter = 0;
-        for (int i = 0; i < sizeOfArrays; i++)
+        for (int i = 0; i < Math.Min(sizeOfArray1,sizeOfArray2); i++)
         {
             if (arrayOne[i]==arrayTwo[i])
             {
@@ -43,6 +50,17 @@ class CompareTwoArrays
                 Console.WriteLine("{0} element of arrays {1} and {2} is different", i + 1,arrayOne[i],arrayTwo[i]);
             }
         }
-        Console.WriteLine("Both arrays have {0} identical elements", counter);
+        if (sizeOfArray1 == sizeOfArray2 && sizeOfArray1 == counter)
+        {
+            Console.WriteLine("The arrays are the same");
+        }
+        else if(counter!=0)
+        {
+            Console.WriteLine("Both arrays have {0} identical elements, but arrays are different", counter);
+        }
+        else
+        {
+            Console.WriteLine("Both arrays do not have identical elements at the same position");
+        }
     }
 }
