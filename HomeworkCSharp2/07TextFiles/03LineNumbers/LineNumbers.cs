2ebbd2b23e132f394a69f0c8c76091c81bd79360
Yordan Bankov <bankoff@mail.bg>
2013-09-12 07:30:19 +0300

    07textFiles

---
+++ HomeworkCSharp2/07TextFiles/03LineNumbers/LineNumbers.cs
@@ -0,0 +1,35 @@
+﻿//Write a program that reads a text file and inserts line numbers in front of each of its lines. 
+//The result should be written to another text file.
+
+using System;
+using System.IO;
+
+class LineNumbers
+{
+    static void Main()
+    {
+        string inputTextFile = @"..\..\TextFile1.txt";
+        string outputTextFile = @"..\..\TextFile2.txt";
+        StreamReader reader = new StreamReader(inputTextFile);
+        StreamWriter writer = new StreamWriter(outputTextFile);
+
+        using (reader)
+        {
+            using (writer)
+            {
+            int lineNumber = 0;
+            string line = reader.ReadLine();
+
+                while (line != null)
+                {
+                    lineNumber++;
+                    writer.WriteLine("Line {0}: {1}", lineNumber, line);
+                    line = reader.ReadLine();
+                }
+            }
+        }
+        Console.WriteLine("Input file is TextFile1.txt!");
+        Console.WriteLine("Output file with inserting line numbers is TextFile2.txt!");
+        Console.WriteLine("You can find the files in directory of the current project!");
+    }
+}
---
+++ HomeworkCSharp2/07TextFiles/09DeleteOddLines/DeleteOddLines.cs
@@ -0,0 +1,48 @@
+﻿//Write a program that deletes from given text file all odd lines. The result should be in the same file.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+class DeleteOddLines
+{
+    static void Main()
+    {
+        PrintResult(ReadEvenLines());
+        Console.WriteLine("Odd lines only were removed!");
+        Console.WriteLine("You can find the file in directory of the current project!");
+    }
+
+    static void PrintResult(List<string> evenRows)
+    {
+        StreamWriter write = new StreamWriter(@"../../TextFile.txt", false, Encoding.GetEncoding("windows-1251"));
+        using (write)
+        {
+            foreach (var row in evenRows)
+            {
+                write.WriteLine(row);
+            }
+        }
+    }
+
+    static List<String> ReadEvenLines()
+    {
+        StreamReader read = new StreamReader(@"../../TextFile.txt", Encoding.GetEncoding("windows-1251"));
+        using (read)
+        {
+            int lineNumber = 1;
+            string current;
+            List<string> evenRows = new List<string>();
+            while ((current = read.ReadLine()) != null)
+            {
+                if (lineNumber % 2 == 0)
+                {
+                    evenRows.Add(current);
+                }
+                lineNumber++;
+            }
+            return evenRows;
+        }
+    }
+}
